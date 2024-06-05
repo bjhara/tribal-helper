c6182402514e8dd382636ef07fbb0bf7fa15d77d
Hampus Ram <hampus.ram@gmail.com>
2024-06-05 20:26:16 +0200

    introduce router

--- src/main.js
+++ src/main.js
@@ -3,14 +3,15 @@ import 'primeicons/primeicons.css'
 import './assets/css/mana.min.css'
 
 import { createApp } from 'vue'
+import router from '@/router'
 import PrimeVue from 'primevue/config'
 import Wind from '@/assets/presets/wind';
 import App from './App.vue'
 
 const app = createApp(App)
+app.use(router)
 app.use(PrimeVue, {
     unstyled: true,
     pt: Wind
 })
-
 app.mount('#app')
