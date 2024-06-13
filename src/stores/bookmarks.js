6b291937ce4866d80398b855ef6b01929bcfe64e
Hampus Ram <hampus.ram@gmail.com>
2024-06-13 14:56:08 +0200

    questionable fix to remove warning in console

--- src/stores/bookmarks.js
+++ src/stores/bookmarks.js
@@ -2,11 +2,11 @@ import { defineStore } from 'pinia'
 import { useLocalStorage } from '@vueuse/core'
 
 export const useBookmarksStore = defineStore('bookmarks', {
-    state: () => {
-        return {
-            bookmarks: useLocalStorage("bookmarks", new Map())
-        }
-    },
+    state: () => ({
+        // TODO: I'm not really sure if we should set listenToStorage changes but it removes a warning
+        // in the console for now...
+        bookmarks: useLocalStorage("bookmarks", new Map(), { listenToStorageChanges: false })
+    }),
     actions: {
         isBookmarked(id) {
             return this.bookmarks.has(id)
